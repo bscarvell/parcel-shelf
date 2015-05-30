6633dd854c7419e79b891705ccf72adc557f8155
Brendan Scarvell <bscarvell@gmail.com>
2015-05-30 12:16:57 +1000

    turned auth off by default

--- www/config.js
+++ www/config.js
@@ -4,7 +4,7 @@ module.exports = {
 	baud: 115200
   },
   security: {
-    requireAuth: true,
+    requireAuth: false,
     password: '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', // sha256 hash
     allowedIPs: [] // Array of IP addresses that don't require authorization
   },
